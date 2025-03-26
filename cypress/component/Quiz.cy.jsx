662cb43db05e48b4a1d5a75ab955a698ca410dc0
jaegerthomas <bjaegerthomas@gmail.com>
2025-03-26 14:44:21 -0400

    initial commit, all workng starter code provided

---
+++ cypress/component/Quiz.cy.jsx
@@ -0,0 +1,9 @@
+import Quiz from "../../client/src/components/Quiz";
+
+describe("Quiz Component", () => {
+  it("should start the quiz and display the first question", () => {});
+
+  it("should answer questions and complete the quiz", () => {});
+
+  it("should restart the quiz after completion", () => {});
+});
---
+++ cypress/e2e/quiz.cy.js
@@ -0,0 +1,7 @@
+describe("Quiz Component", () => {
+  it("should start the quiz and display the first question", () => {});
+
+  it("should answer questions and complete the quiz", () => {});
+
+  it("should restart the quiz after completion", () => {});
+});
