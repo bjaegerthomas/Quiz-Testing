92c1125e213a0702f5f4ccc75c344207c445ddc5
jaegerthomas <bjaegerthomas@gmail.com>
2025-03-26 16:19:43 -0400

    completed first test code for e2e

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,7 +1,23 @@
 describe("Quiz Component", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
   it("should start the quiz and display the first question", () => {});
+    cy.fixture('questions').then((questions) => {
+        cy.intercept('GET', '/api/questions/random', {
+            statusCode: 200,
+            body: fixture
+        }).as('mockGetRequest');
+    });
+    cy.wait('@mockGetRequest').then((intercept) => {
+      assert.isNotNull(intercept.response?.body, 'Response has data');
+      expect(intercept.response?.body).to.have.property('question','Which is the correct answer?');
+    });
 
   it("should answer questions and complete the quiz", () => {});
+   
 
   it("should restart the quiz after completion", () => {});
+    
 });
