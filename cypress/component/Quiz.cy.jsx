28cb452c31b05b2fb524cee28da12e55714238d4
jaegerthomas <bjaegerthomas@gmail.com>
2025-03-26 15:27:28 -0400

    components tests defined

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,9 +1,29 @@
 import Quiz from "../../client/src/components/Quiz";
 
 describe("Quiz Component", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    });
+
   it("should start the quiz and display the first question", () => {});
+    cy.get('button').contains('Start Quiz').click();
+    cy.get('.card').should('be.visible');
+    cy.get('h2').should('not.be.empty');
 
   it("should answer questions and complete the quiz", () => {});
+    cy.get('button').contains('Start Quiz').click();
+    for (let i = 0; i < 10; i++) {
+        cy.get('button').contains('1').click();
+    }
+    cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
 
   it("should restart the quiz after completion", () => {});
+    cy.get('button').contains('Start Quiz').click();
+    for (let i = 0; i < 10; i++) {
+        cy.get('button').contains('1').click();
+    }
+    cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
+    cy.get('button').contains('Take New Quiz').click();
+    cy.get('.card').should('be.visible');
+    cy.get('h2').should('not.be.empty');
 });
