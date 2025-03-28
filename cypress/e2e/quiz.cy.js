adbb6307b15ff62bb56a979062f1032805e5a299
jaegerthomas <bjaegerthomas@gmail.com>
2025-03-28 08:03:43 -0400

    comment out to test first [art of test

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -15,9 +15,18 @@ describe("Quiz Component", () => {
       expect(intercept.response?.body).to.have.property('question','Which is the correct answer?');
     });
 
-  it("should answer questions and complete the quiz", () => {});
-   
+/*  it("should answer questions and complete the quiz", () => {});
+    cy.fixture('questions').then((questions) => {
+      cy.intercept('GET', '/api/questions/random', {
+          statusCode: 200,
+          body: fixture
+      }).as('mockGetRequest');
+    });
+    cy.wait('@mockGetRequest').then((intercept) => {
+      assert.isNotNull(intercept.response?.body, 'Response has data');
+      expect(intercept.response?.body).to.have.property('question','Which is the correct answer?');
+    });
 
-  it("should restart the quiz after completion", () => {});
+  it("should restart the quiz after completion", () => {}); */
     
-});
+}); 
